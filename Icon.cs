14ac592257ba4fde9a1c44fad751445b8f1260fd
Yuuka <62856297+yoshakami@users.noreply.github.com>
2025-09-19 10:26:21 +0200

    fill icon ui functions

--- Icon.cs
+++ Icon.cs
@@ -67,56 +67,6 @@ namespace WaccaSongBrowser
 
         }
 
-        private void filtericonTextureNameCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void filtericonRarityCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void filteritemActivateStartTimeCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void filteritemActivateEndTimeCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void filterGainWaccaPointCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void filtericonNameCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void filtericonAcquisitionWayCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void filterNameTagCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void filterexplanationTextTagCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void filterbIsInitItemEnableCb_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void searchButton_Click(object sender, EventArgs e)
         {
 
@@ -157,54 +107,194 @@ namespace WaccaSongBrowser
 
         }
 
+        private void filtericonTextureNameCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            filtericonTextureNameTextBox.Enabled = filtericonTextureNameCheckBox.Checked;
+        }
+
+        private void filtericonRarityCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            filtericonRarityTextBox.Enabled = filtericonRarityCheckBox.Checked;
+        }
+
+        private void filteritemActivateStartTimeCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            filteritemActivateStartTimeTextBox.Enabled = filteritemActivateStartTimeCheckBox.Checked;
+        }
+
+        private void filteritemActivateEndTimeCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            filteritemActivateEndTimeTextBox.Enabled = filteritemActivateEndTimeCheckBox.Checked;
+        }
+
+        private void filterGainWaccaPointCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            filterGainWaccaPointTextBox.Enabled = filterGainWaccaPointCheckBox.Checked;
+        }
+
+        private void filtericonNameCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            filtericonNameTextBox.Enabled = filtericonNameCheckBox.Checked;
+        }
+
+        private void filtericonAcquisitionWayCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            filtericonAcquisitionWayTextBox.Enabled = filtericonAcquisitionWayCheckBox.Checked;
+        }
+
+        private void filterNameTagCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            filterNameTagTextBox.Enabled = filterNameTagCheckBox.Checked;
+        }
+
+        private void filterexplanationTextTagCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            filterexplanationTextTagTextBox.Enabled = filterexplanationTextTagCheckBox.Checked;
+        }
+
+        private void filterbIsInitItemEnableCb_CheckedChanged(object sender, EventArgs e)
+        {
+            filterbIsInitItemCheckBox.Enabled = filterbIsInitItemEnableCb.Checked;
+        }
+
         private void iconTextureNamefreezeCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (iconTextureNamefreezeCheckBox.CheckState == CheckState.Checked)
+            {
+                iconTextureNameTextBox.BackColor = Color.LightBlue;
+                iconTextureNamefreezeCheckBox.BackColor = Color.LightBlue;
+            }
+            else
+            {
+                iconTextureNameTextBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+                iconTextureNamefreezeCheckBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+            }
         }
 
         private void iconRarityfreezeCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (iconRarityfreezeCheckBox.CheckState == CheckState.Checked)
+            {
+                iconRarityTextBox.BackColor = Color.LightBlue;
+                iconRarityfreezeCheckBox.BackColor = Color.LightBlue;
+            }
+            else
+            {
+                iconRarityTextBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+                iconRarityfreezeCheckBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+            }
         }
 
         private void itemActivateStartTimefreezeChechBox_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (itemActivateStartTimefreezeChechBox.CheckState == CheckState.Checked)
+            {
+                itemActivateStartTimeTextBox.BackColor = Color.LightBlue;
+                itemActivateStartTimefreezeChechBox.BackColor = Color.LightBlue;
+            }
+            else
+            {
+                itemActivateStartTimeTextBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+                itemActivateStartTimefreezeChechBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+            }
         }
 
         private void ItemActivateEndTimefreezeCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (ItemActivateEndTimefreezeCheckBox.CheckState == CheckState.Checked)
+            {
+                itemActivateEndTimeTextBox.BackColor = Color.LightBlue;
+                ItemActivateEndTimefreezeCheckBox.BackColor = Color.LightBlue;
+            }
+            else
+            {
+                itemActivateEndTimeTextBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+                ItemActivateEndTimefreezeCheckBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+            }
         }
 
         private void gainWaccaPointfreezeCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (gainWaccaPointfreezeCheckBox.CheckState == CheckState.Checked)
+            {
+                gainWaccaPointTextBox.BackColor = Color.LightBlue;
+                gainWaccaPointfreezeCheckBox.BackColor = Color.LightBlue;
+            }
+            else
+            {
+                gainWaccaPointTextBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+                gainWaccaPointfreezeCheckBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+            }
         }
 
         private void iconNamefreezeCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (iconNamefreezeCheckBox.CheckState == CheckState.Checked)
+            {
+                iconNameTextBox.BackColor = Color.LightBlue;
+                iconNamefreezeCheckBox.BackColor = Color.LightBlue;
+            }
+            else
+            {
+                iconNameTextBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+                iconNamefreezeCheckBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+            }
         }
 
         private void iconAcquisitionWayfreezeCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (iconAcquisitionWayfreezeCheckBox.CheckState == CheckState.Checked)
+            {
+                iconAcquisitionWayTextBox.BackColor = Color.LightBlue;
+                iconAcquisitionWayfreezeCheckBox.BackColor = Color.LightBlue;
+            }
+            else
+            {
+                iconAcquisitionWayTextBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+                iconAcquisitionWayfreezeCheckBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+            }
         }
 
         private void nameTagfreezeCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (nameTagfreezeCheckBox.CheckState == CheckState.Checked)
+            {
+                nameTagTextBox.BackColor = Color.LightBlue;
+                nameTagfreezeCheckBox.BackColor = Color.LightBlue;
+            }
+            else
+            {
+                nameTagTextBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+                nameTagfreezeCheckBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+            }
         }
 
         private void explanationTextTagfreezeCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (explanationTextTagfreezeCheckBox.CheckState == CheckState.Checked)
+            {
+                explanationTextTagTextBox.BackColor = Color.LightBlue;
+                explanationTextTagfreezeCheckBox.BackColor = Color.LightBlue;
+            }
+            else
+            {
+                explanationTextTagTextBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+                explanationTextTagfreezeCheckBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+            }
         }
 
         private void bIsInitItemfreezeCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (bIsInitItemfreezeCheckBox.CheckState == CheckState.Checked)
+            {
+                bIsInitItem.BackColor = Color.LightBlue;
+                bIsInitItemfreezeCheckBox.BackColor = Color.LightBlue;
+            }
+            else
+            {
+                bIsInitItem.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+                bIsInitItemfreezeCheckBox.BackColor = Color.FromArgb(0xff, 0xf0, 0xf0, 0xf0);
+            }
         }
     }
 }
